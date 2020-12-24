8d50b855fd803ba138067b3ccc2fb23f85361edc
Hao Chen <hao.chen23@outlook.com>
2020-12-25 10:19:06 +1300

    add flow net dataset class

--- models/model_flownet.py
+++ models/model_flownet.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from building_blocks import plainDecoderBlock, plainEncoderBlock, resDecoderBlock, resEncoderBlock
+from .building_blocks import plainDecoderBlock, plainEncoderBlock, resDecoderBlock, resEncoderBlock
 
 
 class EncoderNet(nn.Module):
