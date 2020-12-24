1fe888727223492aeb7c8f07b63471d611715387
Hao Chen <hao.chen23@outlook.com>
2020-12-24 15:58:46 +1300

    Add residual blocks

--- models/model_flownet.py
+++ models/model_flownet.py
@@ -37,6 +37,68 @@ class plainDecoderBlock(nn.Module):
         return x
 
 
+class resEncoderBlock(nn.Module):
+    def __init__(self, inChannel, outChannel, stride):
+
+        super(resEncoderBlock, self).__init__()
+        self.conv1 = nn.Conv2d(inChannel, outChannel, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(outChannel)
+        self.conv2 = nn.Conv2d(outChannel, outChannel, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(outChannel)
+
+        self.downsample = None
+        if stride != 1:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(inChannel, outChannel, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(outChannel))
+
+    def forward(self, x):
+        residual = x
+
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = F.relu(out)
+        return out
+
+
+class resDecoderBlock(nn.Module):
+    def __init__(self, inChannel, outChannel, stride):
+
+        super(resDecoderBlock, self).__init__()
+        self.conv1 = nn.Conv2d(inChannel, inChannel, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(inChannel)
+
+        self.downsample = None
+
+        if stride == 1:
+            self.conv2 = nn.Conv2d(inChannel, outChannel, kernel_size=3, stride=1, padding=1)
+            self.bn2 = nn.BatchNorm2d(outChannel)
+        else:
+            self.conv2 = nn.ConvTranspose2d(inChannel, outChannel, kernel_size=2, stride=2)
+            self.bn2 = nn.BatchNorm2d(outChannel)
+
+            self.downsample = nn.Sequential(
+                nn.ConvTranspose2d(inChannel, outChannel, kernel_size=1, stride=2, output_padding=1),
+                nn.BatchNorm2d(outChannel))
+
+    def forward(self, x):
+        residual = x
+
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = F.relu(out)
+        return out
+
 
 
 
