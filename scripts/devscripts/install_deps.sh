22b0dafe6bd291b8d51da4037878c78f87177e67
Hao Chen <hao.chen23@outlook.com>
2021-01-08 11:41:18 +1300

    add requirements.txt and bash script to install dependencies for CI/CD

---
+++ scripts/devscripts/install_deps.sh
@@ -0,0 +1,18 @@
+BASE_DIR=`pwd`"/"`dirname $0`
+ERRORS=""
+
+function error() {
+  ERR=$1
+  ERRORS="$ERRORS\n$ERR"
+  echo $ERR
+}
+
+function install_deps() {
+  echo "installing requirements"
+  cat $BASE_DIR/*/requirements.txt | \
+    sort -u | \
+    # testing the installed version of torch, so don't pip install it.
+    grep -vE '^torch$' | \
+    pip install -r /dev/stdin || \
+    { error "failed to install dependencies"; exit 1; }
+}
