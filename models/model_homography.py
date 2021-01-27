4fc60c7c6798dbcb96d195501b19b185484782a3
Hao Chen <hao.chen23@outlook.com>
2021-01-27 18:20:18 +1300

    multi_head homography model

--- models/model_homography.py
+++ models/model_homography.py
@@ -8,6 +8,7 @@ from torchvision import models
 from matplotlib import pyplot as plt
 from models.modules.building_blocks import ConvBlock
 
+
 class HomographyNet(nn.Module):
 
     def __init__(self, apply_norm=False, norm_type='BatchNorm',
@@ -18,85 +19,106 @@ class HomographyNet(nn.Module):
             param.requires_grad = False
 
         self.resnet_bridge = nn.Sequential(*list(pretrained_model.children())[:-2])
+        self.conv_branchs = []
+        self.fc_blocks = []
+        self.out_len = out_len
+        for i in range(out_len):
+            self.conv_branchs.append(self.make_conv_branch(apply_norm=apply_norm,
+                                                           norm_type=norm_type,
+                                                           apply_dropout=apply_dropout,
+                                                           drop_out=drop_out))
+            self.fc_blocks.append(self.make_fc_block())
 
-        self.conv1 = ConvBlock(inChannel=2048, outChannel=128, kernel_size=3,
-                               stride=1, padding=1, apply_norm=apply_norm,
-                               norm_type=norm_type, apply_dropout=apply_dropout,
-                               drop_out=drop_out)
+    def make_conv_branch(self, apply_norm, norm_type, apply_dropout, drop_out):
+        conv1 = ConvBlock(inChannel=2048, outChannel=128, kernel_size=3,
+                          stride=1, padding=1, apply_norm=apply_norm,
+                          norm_type=norm_type, apply_dropout=apply_dropout,
+                          drop_out=drop_out)
 
-        self.conv2 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
-                               stride=1, padding=1, apply_norm=apply_norm,
-                               norm_type=norm_type, apply_dropout=apply_dropout,
-                               drop_out=drop_out)
+        conv2 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
+                          stride=1, padding=1, apply_norm=apply_norm,
+                          norm_type=norm_type, apply_dropout=apply_dropout,
+                          drop_out=drop_out)
 
-        self.conv3 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
-                               stride=1, padding=1, apply_norm=apply_norm,
-                               norm_type=norm_type, apply_dropout=apply_dropout,
-                               drop_out=drop_out)
+        conv3 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
+                          stride=1, padding=1, apply_norm=apply_norm,
+                          norm_type=norm_type, apply_dropout=apply_dropout,
+                          drop_out=drop_out)
 
-        self.conv4 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
-                               stride=1, padding=1, apply_norm=apply_norm,
-                               norm_type=norm_type, apply_dropout=apply_dropout,
-                               drop_out=drop_out)
+        conv4 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
+                          stride=1, padding=1, apply_norm=apply_norm,
+                          norm_type=norm_type, apply_dropout=apply_dropout,
+                          drop_out=drop_out)
 
-        self.conv5 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
-                               stride=1, padding=1, apply_norm=apply_norm,
-                               norm_type=norm_type, apply_dropout=apply_dropout,
-                               drop_out=drop_out)
+        conv5 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
+                          stride=1, padding=1, apply_norm=apply_norm,
+                          norm_type=norm_type, apply_dropout=apply_dropout,
+                          drop_out=drop_out)
 
-        self.conv6 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
-                               stride=1, padding=1, apply_norm=apply_norm,
-                               norm_type=norm_type, apply_dropout=apply_dropout,
-                               drop_out=drop_out)
+        conv6 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
+                          stride=1, padding=1, apply_norm=apply_norm,
+                          norm_type=norm_type, apply_dropout=apply_dropout,
+                          drop_out=drop_out)
 
-        self.conv7 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
-                               stride=1, padding=1, apply_norm=apply_norm,
-                               norm_type=norm_type, apply_dropout=apply_dropout,
-                               drop_out=drop_out)
+        conv7 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
+                          stride=1, padding=1, apply_norm=apply_norm,
+                          norm_type=norm_type, apply_dropout=apply_dropout,
+                          drop_out=drop_out)
 
-        self.conv8 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
-                               stride=1, padding=1, apply_norm=apply_norm,
-                               norm_type=norm_type, apply_dropout=apply_dropout,
-                               drop_out=drop_out)
+        conv8 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
+                          stride=1, padding=1, apply_norm=apply_norm,
+                          norm_type=norm_type, apply_dropout=apply_dropout,
+                          drop_out=drop_out)
 
-        self.conv9 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
-                               stride=1, padding=1, apply_norm=apply_norm,
-                               norm_type=norm_type, apply_dropout=apply_dropout,
-                               drop_out=drop_out)
+        conv9 = ConvBlock(inChannel=128, outChannel=128, kernel_size=3,
+                          stride=1, padding=1, apply_norm=apply_norm,
+                          norm_type=norm_type, apply_dropout=apply_dropout,
+                          drop_out=drop_out)
 
-        self.fc_block = nn.Sequential(
-            nn.Linear(6272, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Linear(32, 16),
-            nn.ReLU(),
-            nn.Linear(16, out_len),
-            nn.Sigmoid())
+        return nn.Sequential(
+            conv1,
+            conv2,
+            conv3,
+            conv4,
+            conv5,
+            conv6,
+            conv7,
+            conv8,
+            conv9
+        )
+
+    def make_fc_block(self):
+        return nn.Sequential(
+                             nn.Linear(6272, 1024),
+                             nn.ReLU(),
+                             nn.Linear(1024, 512),
+                             nn.ReLU(),
+                             nn.Linear(512, 256),
+                             nn.ReLU(),
+                             nn.Linear(256, 128),
+                             nn.ReLU(),
+                             nn.Linear(128, 64),
+                             nn.ReLU(),
+                             nn.Linear(64, 32),
+                             nn.ReLU(),
+                             nn.Linear(32, 16),
+                             nn.ReLU(),
+                             nn.Linear(16, 1),
+                             nn.Sigmoid()
+                             )
 
     def forward(self, x):
         x = self.resnet_bridge(x)
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.conv6(x)
-        x = self.conv7(x)
-        x = self.conv8(x)
-        x = self.conv9(x)
-        x = torch.flatten(x, 1)
-        x = self.fc_block(x)
+        outs = []
+        for i in range(self.out_len):
+            branch_output = self.conv_branchs[i](x)
+            branch_output = torch.flatten(branch_output, 1)
+            branch_output = self.fc_blocks[i](branch_output)
+            outs.append(branch_output)
+        print(outs)
+        output = torch.cat(outs, dim=1)
 
-        return x
+        return output
 
     # def print_grad(self, module, grad_input, grad_output):
     #     print('Inside ' + module.__class__.__name__ + ' backward')
@@ -129,8 +151,10 @@ class HomographyNet(nn.Module):
 
 
 if __name__ == '__main__':
-    model = HomographyNet()
+    model = HomographyNet(out_len=3)
     dummy_input = torch.randn([5, 3, 512, 512])
+    model.eval()
+    print(dummy_input)
     output = model(dummy_input)
-    print(output.shape)
-    torch.save(model, 'saved.pt')
+    print(output)
+    # torch.save(model, 'saved.pt')
